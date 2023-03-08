cf7350898902577c79a83a9df73f657ed218566e
Nicolas Powell <nicopowell30@gmail.com>
2023-03-08 20:16:33 -0300

    terminé el ejercicio 13

---
+++ js/ejercicio-13.js
@@ -0,0 +1,6 @@
+// Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+let texto = prompt("Introduce un texto: ");
+let textoMayusculas = texto.toUpperCase();
+
+document.write(`<p>${textoMayusculas}</p>`);
